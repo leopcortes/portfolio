8da8737f3a2565006611ce924763e7e73345bedb
leopcortes <leo.pereira.cortes@gmail.com>
2025-05-03 00:16:24 -0300

    calculadora exati

--- src/components/buttonCalculator.tsx
+++ src/components/buttonCalculator.tsx
@@ -17,7 +17,7 @@ export default function ButtonCalculator({
   return (
     <Link
       href={link}
-      className={`m-[9px] flex w-3/4 cursor-pointer items-center justify-center gap-1.5 self-center justify-self-center rounded-[7px] border-none p-[10px] text-2xl font-normal text-white decoration-transparent drop-shadow-[0px_0px_2px_#101010] hover:-translate-y-[1px] active:translate-y-[2px] active:shadow-[0.5px_0.5px_rgb(59,59,59)] ${color}`}
+      className={`m-[9px] flex w-3/4 cursor-pointer items-center justify-center gap-1.5 self-center justify-self-center rounded-[7px] border-none p-[10px] text-2xl font-normal text-white decoration-transparent drop-shadow-[0px_0px_2px_#101010] transition ease-in-out hover:-translate-y-[1px] active:translate-y-[2px] active:shadow-[0.5px_0.5px_rgb(59,59,59)] ${color}`}
     >
       {icon}
       {text}
